954c98858142240d2208b29e31f39525fb104f9c
HugoYT123 <94163857+HugoYT123@users.noreply.github.com>
2023-02-22 10:55:16 +0100

    Car Script C#
    Recovery Script Uploaded

---
+++ PlayerCar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCar : MonoBehaviour
+{
+    Transform carTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carTransform = GetComponent<Transform>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        carTransform.Translate(0, 0.01f, 0);
+    }
+}
